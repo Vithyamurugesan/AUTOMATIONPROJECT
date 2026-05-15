17f77625ac2556386c557a054b287b3f213c0ac2
haritha <haritharavichandran11@gmail.com>
2026-05-15 13:09:25 +0530

    cucumber runner

--- DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
+++ DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/Features/Login.feature",
         glue = "com.stepdefinitions",
-        tags=("@smoke"),
+        //tags=("@smoke"),
         plugin = {
                 "pretty",
                 "html:target/cucumber-reports/cucumber.html",
