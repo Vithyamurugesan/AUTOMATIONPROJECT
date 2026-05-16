a230d1e4d90abd4effc3bfae74b6fcd5c8217e68
sowndariya <sowndariyadeveloper@gmail.com>
2026-05-16 19:45:37 +0530

    Update the cart feature

--- DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
+++ DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
@@ -4,8 +4,6 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-
-
         features ="src/test/resources/Features/Cart.feature",
         glue = "com.stepdefinitions",
         plugin = {
