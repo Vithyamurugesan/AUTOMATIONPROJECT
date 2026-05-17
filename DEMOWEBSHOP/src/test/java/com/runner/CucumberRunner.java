366f4eca2d3ed4dfb18786a3b2e29be8570ef299
Vithyamurugesan <vithyabtech54@gmail.com>
2026-05-17 23:44:08 +0530

    Resolved merge conflicts with main

--- DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
+++ DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
 
-        features = "src/test/resources/Features/Register.feature",
+        features = "src/test/resources/Features/",
         glue = "com.stepdefinitions",
         plugin = {
                 "pretty",
