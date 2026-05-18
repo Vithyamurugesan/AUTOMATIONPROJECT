b9705b15eaa569cb6191d5f3bd2b85f269363fd5
haritha <haritharavichandran11@gmail.com>
2026-05-18 12:57:19 +0530

    new commit

---
+++ DEMOWEBSHOP/src/test/java/com/runner/FailedRunner.java
@@ -0,0 +1,26 @@
+package com.runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+
+        features =
+        "@target/failed_scenarios.txt",
+
+        glue =
+        "com.stepdefinitions",
+
+        plugin = {
+                "pretty",
+                "html:target/rerun-report.html",
+                "json:target/rerun.json"
+        },
+
+        monochrome = true
+)
+
+public class FailedRunner
+extends AbstractTestNGCucumberTests {
+
+}
