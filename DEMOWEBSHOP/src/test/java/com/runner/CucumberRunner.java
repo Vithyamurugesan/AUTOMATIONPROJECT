159b836ce0384337eaaa9f7a6451d845dcf830f3
jeevapranesh005 <jeevapranesh.vrp@gmail.com>
2026-05-19 22:12:09 +0530

    Updated checkout feature files

--- DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
+++ DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
@@ -14,7 +14,7 @@ import io.cucumber.testng.CucumberOptions;
                 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
                 "rerun:target/failed_scenarios.txt"
         },
-       tags="@scenario_6",
+       //tags="@scenario_6",
         monochrome = true
 )
 public class CucumberRunner extends AbstractTestNGCucumberTests {
