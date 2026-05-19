ae4a2ceac595df34055621e0692ce8ed467b643e
sowndariya <sowndariyadeveloper@gmail.com>
2026-05-19 06:38:07 +0530

    product details feature updated

--- DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
+++ DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
 
-        features ="src/test/resources/Features/",
+        features ="src/test/resources/Features/ProductDetails.feature",
         glue = "com.stepdefinitions",
         plugin = {
                 "pretty",
