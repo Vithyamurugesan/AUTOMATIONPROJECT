d3c08a21f1530d12a3458c1e30ed68bd015d9b12
haritha <haritharavichandran11@gmail.com>
2026-05-20 08:24:16 +0530

    orde cucmber

--- DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
+++ DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
 
-        features ="src/test/resources/Features/Login.feature",
+        features ="src/test/resources/Features/Order.feature",
         glue = "com.stepdefinitions",
  
         plugin = {
