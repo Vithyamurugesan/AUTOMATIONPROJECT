6a8e91830b0981b0e573b061182ca1e3eb3e92ae
jeevapranesh005 <jeevapranesh.vrp@gmail.com>
2026-05-20 16:30:45 +0530

    Updated CucumberRunner

--- DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
+++ DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
@@ -1,6 +1,6 @@
 package com.runner;
 
-import org.testng.annotations.DataProvider;
+import org.testng.annotations.DataProvider ;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
@@ -8,9 +8,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 
 		
-        features ="src/test/resources/Features/",
+        features ="src/test/resources/Features/checkout.feature",
         glue = "com.stepdefinitions",
-        tags="@jeeva",
         plugin = {
                 "pretty",
                 "html:target/cucumber-reports/cucumber.html",
@@ -19,16 +18,16 @@ import io.cucumber.testng.CucumberOptions;
                 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
                 "rerun:target/failed_scenarios.txt"
         },
-       
+        tags="@BillingValidation",
         monochrome=true
      )
 
 public class CucumberRunner extends AbstractTestNGCucumberTests {
 
-    @Override
-    @DataProvider(parallel = true)
-    public Object[][] scenarios() {
-        return super.scenarios();
-    }
+//    @Override
+//    @DataProvider(parallel = true)
+//    public Object[][] scenarios() {
+//        return super.scenarios();
+//    }
 	
 }
