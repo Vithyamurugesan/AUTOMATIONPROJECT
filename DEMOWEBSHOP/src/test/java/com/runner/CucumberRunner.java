12724bf9a3cf0bef22aa7fa6adcb40a0e3409371
Jeeva Pranesh P <166803755+jeevapranesh005@users.noreply.github.com>
2026-05-21 22:11:09 +0530

    Add tags for LoginCheckout in CucumberRunner

--- DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
+++ DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
@@ -19,6 +19,7 @@ import io.cucumber.testng.CucumberOptions;
                 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
                 "rerun:target/failed_scenarios.txt"
         },
+		tags="@LoginCheckout",
         monochrome=true
         
      )
