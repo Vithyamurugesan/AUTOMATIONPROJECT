ce74907d5f4fe32891170a4db3f5c1eb4760e2e5
Vithyamurugesan <vithyabtech54@gmail.com>
2026-05-22 10:28:55 +0530

    Change in runner

--- DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
+++ DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
@@ -10,7 +10,6 @@ import io.cucumber.testng.CucumberOptions;
 		
         features ="src/test/resources/Features/",
         glue = "com.stepdefinitions",
-        tags="@SowndariyaCart or @SowndariyaProductDetail",
         plugin = {
                 "pretty",
                 "html:target/cucumber-reports/cucumber.html",
