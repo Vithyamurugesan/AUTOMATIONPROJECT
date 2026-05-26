1d1b0d067022e7f7d8606b4c0e662058c2b4f6d0
Vithyamurugesan <vithyabtech54@gmail.com>
2026-05-26 22:50:53 +0530

    Locators Updated

--- DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
+++ DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
 
-        features ="src/test/resources/Features/Register.feature",
+        features ="src/test/resources/Features/Catlog.feature",
         glue = "com.stepdefinitions",
         plugin = {
                 "pretty",
