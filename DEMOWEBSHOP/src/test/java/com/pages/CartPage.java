243458c6f9e7746be80ac74f2bae0fe4b009260f
Vithyamurugesan <vithyabtech54@gmail.com>
2026-05-26 23:04:17 +0530

    Conflict resolved

--- DEMOWEBSHOP/src/test/java/com/pages/CartPage.java
+++ DEMOWEBSHOP/src/test/java/com/pages/CartPage.java
@@ -6,39 +6,40 @@ public class CartPage {
 
 	private By books = By.linkText("Books");
 
-	private By computingAndInternetBook = By.linkText("Computing and Internet");
+	private By computingAndInternetBook = By.xpath("//h2[@class='product-title']/child::a[normalize-space()='Computing and Internet']");
 
 	private By shoppingCart = By.xpath("//span[text()='Shopping cart']/ancestor::a");
 
 	private By addToCartButton = By.xpath("//input[contains(@value,'Add to cart')]");
 
-	private By cartTable = By.cssSelector("table.cart");
+	private By cartTable = By.xpath("//form[@action='/cart']/descendant::table");
 
-	private By productList = By.cssSelector("td.product");
+	private By productList = By.xpath("//table/descendant::tbody/child::tr/child::td[@class='product']");
 
 	private By subtotal = By.xpath("//span[contains(@class,'product-price')]");
 
 	private By couponBox = By.name("discountcouponcode");
 
-	private By couponButton = By.name("applydiscountcouponcode");
+	private By couponButton = By.xpath("//input[@name='discountcouponcode']/following-sibling::input");
 
-	private By giftCardButton = By.name("applygiftcardcouponcode");
-	
 	private By giftCardBox = By.name("giftcardcouponcode");
-	
-	private By message = By.cssSelector("div.message");
+
+	private By giftCardButton = By.xpath("//input[@name='giftcardcouponcode']/following-sibling::input");
+
+	private By message = By.xpath("//div[@class='message']");
 
 	private By emptyCartMsg = By.cssSelector(".order-summary-content");
 
 	private By quantityBox = By.cssSelector("input.qty-input");
 
-	private By updateCartButton = By.name("updatecart");
+	private By updateCartButton = By.xpath("//input[@name='updatecart']");
 
-	private By removeCheckBox = By.name("removefromcart");
+	private By removeCheckBox = By.xpath("//input[@name='removefromcart']");
 
 	public By getBooks() {
 		return books;
 	}
+
 	public By getComputingBook() {
 		return computingAndInternetBook;
 	}
