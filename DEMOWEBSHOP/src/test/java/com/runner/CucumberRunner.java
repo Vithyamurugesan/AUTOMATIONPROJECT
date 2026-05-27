fbc782768877432f524bf647b1c4f0e8342f16e7
haritha <haritharavichandran11@gmail.com>
2026-05-27 23:11:16 +0530

    tag added in download

--- DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
+++ DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
@@ -9,6 +9,7 @@ import io.cucumber.testng.CucumberOptions;
 
         features ="src/test/resources/Features/",
         glue = "com.stepdefinitions",
+        tags="@harithaDownload",
         plugin = {
                 "pretty",
                 "html:target/cucumber-reports/cucumber.html",
