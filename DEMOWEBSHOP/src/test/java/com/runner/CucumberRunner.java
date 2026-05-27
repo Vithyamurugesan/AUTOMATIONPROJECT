e858d93fed8fe3d9e4caa769a53416d238bbece0
haritha <haritharavichandran11@gmail.com>
2026-05-27 23:06:16 +0530

    added the tag

--- DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
+++ DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
@@ -9,6 +9,7 @@ import io.cucumber.testng.CucumberOptions;
 
         features ="src/test/resources/Features/",
         glue = "com.stepdefinitions",
+        tags="@harithaLogin",
         plugin = {
                 "pretty",
                 "html:target/cucumber-reports/cucumber.html",
