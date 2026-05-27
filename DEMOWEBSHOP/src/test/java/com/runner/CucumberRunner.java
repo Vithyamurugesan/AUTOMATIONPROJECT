bec6f3bd14cf928aece9e0cae48f61a940cf3eda
SOWNDARIYA K <146727613+sowndariya-k@users.noreply.github.com>
2026-05-27 11:21:31 +0530

    Update CucumberRunner to include specific tags

--- DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
+++ DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
@@ -9,6 +9,7 @@ import io.cucumber.testng.CucumberOptions;
 
         features ="src/test/resources/Features/",
         glue = "com.stepdefinitions",
+	tags=@SowndariyaCart or @SowndariyaProductDetail,
         plugin = {
                 "pretty",
                 "html:target/cucumber-reports/cucumber.html",
@@ -17,8 +18,6 @@ import io.cucumber.testng.CucumberOptions;
                 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
                 "rerun:target/failed_scenarios.txt"
         },
-
-		tags="not @Ignore",
 		
         monochrome=true
         
