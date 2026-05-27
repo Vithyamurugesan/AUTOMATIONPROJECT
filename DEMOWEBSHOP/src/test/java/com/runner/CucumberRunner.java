c30ebdcb59ca960c4c9b0c69b5ce2556496f2c40
SOWNDARIYA K <146727613+sowndariya-k@users.noreply.github.com>
2026-05-27 11:53:11 +0530

    Fix Cucumber tags syntax in CucumberRunner.java

--- DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
+++ DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
@@ -9,7 +9,7 @@ import io.cucumber.testng.CucumberOptions;
 
         features ="src/test/resources/Features/",
         glue = "com.stepdefinitions",
-	tags=@SowndariyaCart or @SowndariyaProductDetail,
+	    tags="@SowndariyaCart or @SowndariyaProductDetail" ,
         plugin = {
                 "pretty",
                 "html:target/cucumber-reports/cucumber.html",
