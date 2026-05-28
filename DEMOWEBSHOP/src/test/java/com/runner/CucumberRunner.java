407b4af3e8b91b0e61aacdfd5e2616cef0513fd2
Vithyamurugesan <vithyabtech54@gmail.com>
2026-05-28 08:17:09 +0530

    Update CucumberRunner.java

--- DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
+++ DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
 
-        features ="src/test/resources/Features/GiftCard.feature",
+        features ="src/test/resources/Features/",
         glue = "com.stepdefinitions",
         plugin = {
                 "pretty",
