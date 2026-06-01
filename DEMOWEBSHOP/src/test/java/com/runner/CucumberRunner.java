2062939d8781844af8442af67bcac08e3886f5a9
Jeeva Pranesh P <166803755+jeevapranesh005@users.noreply.github.com>
2026-06-01 12:36:11 +0530

    Update CucumberRunner.java

--- DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
+++ DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
 
-        features ="src/test/resources/Features/",
+        features ="src/test/resources/Features/checkout.feature",
         glue = "com.stepdefinitions",
      tags="@jeeva",
         plugin = {
