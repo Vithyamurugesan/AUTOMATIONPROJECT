b55d79914fbd1edf9b9eee4ceba95b2c5d43db4f
Vithyamurugesan <vithyabtech54@gmail.com>
2026-06-03 12:28:20 +0530

    Update CucumberRunner.java

--- DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
+++ DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
@@ -9,7 +9,7 @@ import io.cucumber.testng.CucumberOptions;
 
         features ="src/test/resources/Features/",
         glue = "com.stepdefinitions",
-        tags="not @Ignore",
+        tags="@Vithya",
         plugin = {
                 "pretty",
                 "html:target/cucumber-reports/cucumber.html",
