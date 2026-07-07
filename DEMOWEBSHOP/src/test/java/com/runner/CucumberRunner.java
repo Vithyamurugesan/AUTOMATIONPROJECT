2eeb790207f85ab9f5f019655078d98996093d65
Vithyamurugesan <vithyabtech54@gmail.com>
2026-07-07 07:43:58 +0530

    Update CucumberRunner.java

--- DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
+++ DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
@@ -8,9 +8,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 
         features ="src/test/resources/Features/",
-        tags ="@SowndariyaNewsletter",
         glue = "com.stepdefinitions",
-        tags="@harithaContact",
+       
         plugin = {
                 "pretty",
                 "html:target/cucumber-reports/cucumber.html",
