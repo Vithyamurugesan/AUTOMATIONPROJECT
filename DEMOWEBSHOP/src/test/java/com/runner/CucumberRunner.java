f17f87dc0c750eea6a569c5820c59f9802b6f1db
Vithyamurugesan <vithyabtech54@gmail.com>
2026-08-14 19:47:30 +0530

    Remove tag from CucumberRunner configuration
    Removed the tag '@Sowndariya' from Cucumber options.

--- DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
+++ DEMOWEBSHOP/src/test/java/com/runner/CucumberRunner.java
@@ -8,7 +8,6 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 
         features ="src/test/resources/Features/",
-        tags ="@Sowndariya",
         glue = "com.stepdefinitions",
        
         plugin = {
